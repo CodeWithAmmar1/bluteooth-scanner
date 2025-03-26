11d67b14bf408ca870eb54283e4c14b801a64a26
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-03-27 02:15:05 +0500

    instructions

--- lib/main.dart
+++ lib/main.dart
@@ -17,3 +17,13 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// package 
+//  permission_handler: ^11.0.0
+//   flutter_reactive_ble: ^5.4.0
+
+//Permission in manifest
+    //  <uses-permission android:name="android.permission.BLUETOOTH"/>
+    // <uses-permission android:name="android.permission.BLUETOOTH_ADMIN"/>
+    // <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+    // <uses-permission android:name="android.permission.FOREGROUND_SERVICE"/>
